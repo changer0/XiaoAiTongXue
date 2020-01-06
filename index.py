cee24c090fe2dbaaa3bf73268f3d086daace9f73
p_zlulzhang <zhanglulu@yuewen.com>
2020-01-06 14:25:33 +0800

    format code

--- index.py
+++ index.py
@@ -3,32 +3,35 @@
 import sys
 import json
 import logging
+
 print('Loading function')
 logger = logging.getLogger()
+
+
 def main_handler(event, context):
     logger.info("start main handler")
-    #print(event)
+    # print(event)
 
     body = json.loads(event['body'])
     query = body['query']
     print(query)
 
-    speakText = query
+    speak_text = query
     if query == "打开学你说话".decode("utf-8") or query == "进入学你说话".decode("utf-8"):
-        speakText = " " # 如果是启动语，不说话
+        speak_text = " "  # 如果是启动语，不说话
 
     response = {
         "version": "1.0",
-        "is_session_end":False,
-            "response": {
-            "open_mic":True,
+        "is_session_end": False,
+        "response": {
+            "open_mic": True,
             "to_speak": {
                 "type": 0,
-                "text": speakText
+                "text": speak_text
             },
             "to_display": {
                 "type": 0,
-                "text": speakText
+                "text": speak_text
             }
         }
 
--- test.py
+++ test.py
@@ -6,20 +6,20 @@ b = "进入学你说话"
 
 print(a == b)
 
-if a == b or True :
+if a == b or True:
     print("相等了")
 else:
     print("不相等")
-query = a.decode("utf-8")
+query = a
 
 speakText = query
-if query == "打开学你说话".decode("utf-8") or query == "进入学你说话".decode("utf-8"):
+if query == "打开学你说话" or query == "进入学你说话":
     speakText = " "
 response = {
     "version": "1.0",
-    "is_session_end":False,
+    "is_session_end": False,
     "response": {
-        "open_mic":True,
+        "open_mic": True,
         "to_speak": {
             "type": 0,
             "text": speakText
